41ba15da1f33c462fde6c2018bed5cffccd58120
ItsME-TM <thushanmalaka123@gmail.com>
2025-12-17 20:14:52 +0530

    Fix import typo in getUserAppointmentStats and await currentUser in DentalHealthOverview component

--- src/components/dashboard/DentalHealthOverview.tsx
+++ src/components/dashboard/DentalHealthOverview.tsx
@@ -1,11 +1,12 @@
-import { getUserAppoinmentStats } from '@/lib/actions/appointments'
+
+import { getUserAppointmentStats } from '@/lib/actions/appointments';
 import { currentUser } from '@clerk/nextjs/server';
 
 import React from 'react'
 
 async function DentalHealthOverview() {
     const appointmentStatus = await getUserAppointmentStats();
-    const user = currentUser();
+    const user = await currentUser();
     
   return (
     <div>
